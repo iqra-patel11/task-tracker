c71d53829120d49a331d03812e0394bcc0ea2870
iqra-patel11 <iqrapatel1111@gmail.com>
2025-07-05 02:54:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';  // ✅ points to styles/index.css
+import './styles/index.css'; // Correct path!
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
